06f7f83d95e57dbd4ac68ec672647c79b9aeeb61
fa1478-007 <fa1478@srmist.edu.in>
2026-03-12 17:08:01 +0530

    Add files via upload
    OOPS banner app UC-3 uploaded

---
+++ OOPSBannerApp.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * This use case improves upon UC3 by storing banner lines in a
+ * String array and printing them using a for-each loop,
+ * improving modularity and reusability.
+ *
+ * @author Developer
+ * @version 3.0
+ */
+
+
+public static void main(String[] args) {
+
+    System.out.println(String.join(" ",
+            " *** ",
+            " *** ",
+            " ***** ",
+            " ***** "
+    ));
+
+    System.out.println(String.join(" ",
+            "**   **",
+            "**   **",
+            "**     ",
+            "**     "
+    ));
+
+    System.out.println(String.join(" ",
+            "**   **",
+            "**   **",
+            "*****  ",
+            "*****  "
+    ));
+
+    System.out.println(String.join(" ",
+            "**   **",
+            "**   **",
+            "**      ",
+            "     **"
+    ));
+
+    System.out.println(String.join(" ",
+            "**   **",
+            "**   **",
+            "**      ",
+            "     **"
+    ));
+
+    System.out.println(String.join(" ",
+            " *** ",
+            " *** ",
+            " ***** ",
+            " ***** "
+    ));
+}
